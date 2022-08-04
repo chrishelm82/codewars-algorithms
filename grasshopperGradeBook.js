f839a20067de50da9ccdfcac8eb3af8f0ab9da89
Chris Helm <helm.chris82@gmail.com>
2022-08-04 16:34:21 -0400

    modified formatting and got rid of some comments

--- grasshopperGradeBook.js
+++ grasshopperGradeBook.js
@@ -11,18 +11,14 @@
 // 0 <= score < 60	'F'
 // Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
 
-
 // SOLUTION
-function getGrade (s1, s2, s3) {
-    // Code here
-    
-    // Declare a variable that gives the mean of s1, s2 and s3. 
-    const meanNumber = (s1 + s2 + s3) / 3
-    
-    if(meanNumber >= 90) return 'A'
-    else if(meanNumber >= 80) return 'B'
-    else if(meanNumber >= 70) return 'C'
-    else if(meanNumber >= 60) return 'D'
-    else if(meanNumber <= 60) return 'F'
-    else console.log('No grade given')
-  }
+function getGrade(s1, s2, s3) {
+  const meanNumber = (s1 + s2 + s3) / 3;
+
+  if (meanNumber >= 90) return "A";
+  else if (meanNumber >= 80) return "B";
+  else if (meanNumber >= 70) return "C";
+  else if (meanNumber >= 60) return "D";
+  else if (meanNumber <= 60) return "F";
+  else console.log("No grade given");
+}
--- wellOfIdeas.js
+++ wellOfIdeas.js
@@ -7,53 +7,50 @@
 
 // The solution should be case insensitive (ie good, GOOD and gOOd all count as a good idea). All inputs may not be strings.
 
-function well(arrayOfIdeas){
-  
-    // initialize count of good at 0
-    let countOfGood = 0;
-    
-    // iterate through arrayOfIdeas
-    for(const subArray of arrayOfIdeas){
-      //iterate through each subarray
-      for(let ele of subArray){
-        //check if element is a string 
-        if(typeof ele === 'string'){
-          // normalize the casing 
-          ele = ele.toLowerCase();
-          // if normalized string equals 'good'
-          if(ele === 'good'){
-              //increment count by 1
-              countOfGood++;
-              // if count > 2 break
-            }
-          }
+function well(arrayOfIdeas) {
+  // initialize count of good at 0
+  let countOfGood = 0;
+
+  // iterate through arrayOfIdeas
+  for (const subArray of arrayOfIdeas) {
+    //iterate through each subarray
+    for (let ele of subArray) {
+      //check if element is a string
+      if (typeof ele === "string") {
+        // normalize the casing
+        ele = ele.toLowerCase();
+        // if normalized string equals 'good'
+        if (ele === "good") {
+          //increment count by 1
+          countOfGood++;
+          // if count > 2 break
         }
       }
-    
-    // if count equals 0 return 'Fail!'
-    if(countOfGood === 0) return 'Fail!';
-    if(countOfGood < 3 ) return 'Publish!';
-    return 'I smell a series!'; 
-  
+    }
   }
-  
-  // read the problem aloud 
-  // reiterate the problem in your own words
-  // ask clarifying questions
-  // give examples 
-  // [] => 'Fail!' // simplest valid input 
-  // [['good']] => 'Publish!'
-  // [['good', 'bad']] => 'Publish!'
-  // [['good', 'bad', 'good', 'good']] => 'I smell a series!'
-  // [['good', 'bad'], ['good', 'BAD']] => 'Publish!'
-  // [['good', 'bad'], ['good', 'BAD'], ['gOOd'], ['bad']] => 'I smell a series!'
-  // [['bad', 'bad'], ['bad', 'BAD'], ['BAD']] => 'Fail!'
-  // [['good', true]] => 'Publish!'
-  // [['good', {}], [[]], [2,3]] => 'Publish!'
-  
-  
-  // write our function signature
-  // write our psuedocode
-  // write a test 
-  // write code to make the test pass
-  // repeat the previous 2 steps until youre done
+
+  // if count equals 0 return 'Fail!'
+  if (countOfGood === 0) return "Fail!";
+  if (countOfGood < 3) return "Publish!";
+  return "I smell a series!";
+}
+
+// read the problem aloud
+// reiterate the problem in your own words
+// ask clarifying questions
+// give examples
+// [] => 'Fail!' // simplest valid input
+// [['good']] => 'Publish!'
+// [['good', 'bad']] => 'Publish!'
+// [['good', 'bad', 'good', 'good']] => 'I smell a series!'
+// [['good', 'bad'], ['good', 'BAD']] => 'Publish!'
+// [['good', 'bad'], ['good', 'BAD'], ['gOOd'], ['bad']] => 'I smell a series!'
+// [['bad', 'bad'], ['bad', 'BAD'], ['BAD']] => 'Fail!'
+// [['good', true]] => 'Publish!'
+// [['good', {}], [[]], [2,3]] => 'Publish!'
+
+// write our function signature
+// write our psuedocode
+// write a test
+// write code to make the test pass
+// repeat the previous 2 steps until youre done
